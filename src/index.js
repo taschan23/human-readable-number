0bd99840a7556299f1648c93faf747c89ee5ada6
evgeny <bakalov.es@ya.ru>
2021-03-09 14:40:03 +0300

    feat: human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let result = '';
+    let names = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten'];
+    let tenNames = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+    let tensNames = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let numberArr = number.toString().split('');
+
+    for (let i = 0; i < numberArr.length; i++) {
+        if (numberArr.length === 3) {
+            result += names[numberArr[0]] + ' hundred';
+            if (parseInt(numberArr[1]) > 0 && parseInt(numberArr[2]) >= 0) {
+                result += ' ';
+            }
+            numberArr = numberArr.splice(1);
+        } else if (numberArr.length === 2) {
+            if (parseInt(numberArr[1]) === 0) {
+                if (parseInt(numberArr[0]) === 1) {
+                    result += names[numberArr.join('')];
+                } else {
+                    result += tensNames[numberArr[0]];
+                }
+            } else {
+                if (parseInt(numberArr[0]) === 1) {
+                    result += tenNames[numberArr[1]];
+                } else {
+                    result += tensNames[numberArr[0]] + ' ' + names[numberArr[1]];
+                }
+            }
+            numberArr = numberArr.splice(1);
+        } else {
+            result = names[numberArr[i]];
+        }
+    }
+
+    return result.trim();
 }
